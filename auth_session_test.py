d788b5c56c1fcee6e03d154e5be2ab7c58f49e17
Kiko Čabala <kristiancabala@gmail.com>
2019-12-27 15:38:00 +0100

    removed current totp (getting it from phone)

--- auth_session_test.py
+++ auth_session_test.py
@@ -2,11 +2,11 @@ from src.authenticator import authenticate
 from src.Recognizer import Recognizer
 from src.totp_generator import get_topt
 
-print("curr topt is {}".format(get_topt()))
+# print("curr topt is {}".format(get_topt()))
 r = Recognizer()
 transcription = r.listenAndTranscribe()
 
 if (authenticate(transcription)):
-  print("AUTH SUCCESSFUL")
+  print("AUTH SUCCESSFUL \n CACTUS: STROKED \n STRAIGHT: UP")
 else:
   print("you done fucked up")
