0d36f26ba15a2db2e099878ab45c390b1bccd722
Kiko Čabala <kristiancabala@gmail.com>
2019-12-27 15:50:54 +0100

    now parsing totp as string, always 6 chars

--- src/authenticator.py
+++ src/authenticator.py
@@ -1,10 +1,15 @@
 from src.totp_generator import get_topt
+from src.config import loadConfig
 
 def authenticate(transcription):
   assert(isinstance(transcription, str))
+  config = loadConfig()['totp']
   
-  topt = get_topt()
-  attempted_pass = int("".join(transcription.split(" ")))
-  print(attempted_pass, topt)
+  topt = str(get_topt())
+  while len(topt) != config['length']:
+    p = "0" + topt
+    topt = p
+
+  attempted_pass = "".join(transcription.split(" "))
 
   return attempted_pass == topt
